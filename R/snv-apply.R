c528b5911b13629a5d8a4888276ba2fde57f4eec
Philipp Baumann <baumann-philipp@proton.me>
2023-08-25 15:17:51 +0200

    basic template

--- R/snv-apply.R
+++ R/snv-apply.R
@@ -21,13 +21,44 @@ snv_apply <- function(X,
   } else {
     spc_list <- dt_prep_sets$spc_prep
   }
+
+  # multivariate apply to map over the inputs for repeated application of
+  # the standard normal variate to already preprocessed sets.
+  # If `spc_list` has length 1, it is repeated to fit the number of rows
+  # supplied in the preprocessing plan. Different modes of parallelization are
+  # available, which can be controlled in the global environment of the user
+  # via `future::plan()`
+  spc_prep_list <- future.apply::future_lapply(
+    spc_list,
+    snv_impl,
+    future.seed = 1L
+  )
+
+  if (is.null(dt_prep_sets)) {
+    # Prepare output;
+    # fresh preprocessing labels; no parameter necessary
+    dt_out <- data.table(
+      prep_set = "snv", prep_label = "snv", prep_params = NULL
+    )
+    # add list of spectra processed with Standard Normal Variate (SNV)
+    # as list-column
+    dt_out[, spc_prep := spc_prep_list]
+  } else {
+
+  }
 }
 
-# nolint start
-snv_impl_base <- function(X) {
-  X <- sweep(as.matrix(X), 1L, rowMeans(as.matrix(X), na.rm = TRUE), `-`)
-  X_sc <- sweep(X, 1L, apply(X, 1, sd, na.rm = TRUE), `/`)
-  return(X_sc)
+snv_impl <- function(X) {
+  if (!is.matrix(X)) {
+    stopifnot(
+      "`X` needs to be either a matrix or a data frame" =
+        is.data.frame(X)
+    )
+    X <- as.matrix(X)
+  }
+  X <- sweep(X, 1L, matrixStats::rowMeans2(X, na.rm = TRUE), `-`)
+  X <- sweep(X, 1L, matrixStats::rowSds(X, na.rm = TRUE), `/`)
+  return(X)
 }
 
 checkset_dt_prep_sets <- function(dt_prep_sets) {
