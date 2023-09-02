a9cd71e00dfd57583cdc8485f701a81728443df7
Philipp Baumann <baumann-philipp@proton.me>
2023-09-02 15:26:34 +0200

    remove reticulate

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,3 @@
 #' @import data.table
 #' @importFrom future.apply future_lapply future_Map
 if (getRversion() >= "2.15.1") utils::globalVariables(c(":="))
-
-# global reference to zarr (will be initialized in .onLoad)
-zarr <- NULL
-
-.onLoad <- function(libname, pkgname) {
-  # use superassignment to update global reference to scipy
-  scipy <<- reticulate::import("zarr", delay_load = TRUE)
-}
